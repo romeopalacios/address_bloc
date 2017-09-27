301f39835327e78fecd68a876422fd7757c4a438
Romeo Palacios <romeopalacios@ymail.com>
2017-09-27 00:55:51 -0500

    added rspec test for entries_2

--- models/address_book.rb
+++ models/address_book.rb
@@ -44,4 +44,16 @@ require "csv"
      end
   end
 
+  def import_from_csv_2(file_name)
+ # Implementation goes here
+
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+   # #8
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+  end
+
 end
