f8a36fd424df2396aa783cb3e5356ef2d9c7436e
Romeo Palacios <romeopalacios@ymail.com>
2017-09-30 18:53:21 -0700

    added binary searches

--- models/address_book.rb
+++ models/address_book.rb
@@ -44,7 +44,7 @@ require "csv"
      end
   end
 
-  def import_from_csv_2(file_name)
+  def import_from_csv_2 (file_name)
  # Implementation goes here
 
      csv_text = File.read(file_name)
@@ -54,6 +54,31 @@ require "csv"
        row_hash = row.to_hash
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
-  end
 
+     # Search AddressBook for a specific entry by name
+ def binary_search(name)
+     # #1
+     lower = 0
+     upper = entries.length - 1
+
+     # #2
+     while lower <= upper
+       # #3
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+
+       # #4
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+     # #5
+     return nil
+  end
+end
 end
